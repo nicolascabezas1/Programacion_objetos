812577c3fa3a2232262e5c10bb40af787d3e0472
Nicolás Cabezas <cabezasn628@gmail.com>
2023-05-10 16:54:18 -0500

    :sparkles: ([java and php docs]) updating java and php documents
    Update of java and php documents in the repository paths. class functionality and inheritance in both languages

---
+++ java/Uberx.java
@@ -0,0 +1,10 @@
+class Uberx extends Car {
+  String brand;
+  String model;
+
+  public Uberx(String license, Account driver, String brand, String model) {
+    super(license, driver);
+    this.brand = brand;
+    this.model = model;
+  }
+}
